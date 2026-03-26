8a7aa17d055ef933ae5df740f2ab12e168c54128
Tiago Cabral <tiagocabral.gja@gmail.com>
2026-03-26 20:43:13 -0300

    aula01

---
+++ aula01/exemplo01/exemplo01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace exemplo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //O comando a baixo espera alguma tecla ser digitada para fechar o programa
+            Console.ReadKey();
+        }
+    }
+}
---
+++ aula01/exemplo02/exemplo02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace exemplo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaração as variaveis
+            double n1, n2, soma, sub, mult, div;
+            // exibir mensagens e ler números de entrada
+            Console.Write("Digite um número: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite outro: ");
+            n2 = double.Parse(Console.ReadLine());
+            // faço o calculos e atribuo o valores para as variaveis
+            soma = n1 + n2;
+            sub = n1 - n2;
+            mult = n1 * n2;
+            div = n1 / n2;
+            // exibir saidas
+            Console.WriteLine($"A soma é {soma}");
+            Console.WriteLine("A subtração é {0}", sub);
+            Console.WriteLine("O prouto é {0}", mult);
+            Console.WriteLine("A razão é {0}", div);
+            Console.WriteLine("o resto é {0}", n1 % n2);
+
+            if (n1 % n2 == 0)
+            {
+                Console.WriteLine("{0} é multiplo de {1}", n1, n2);
+            }
+        }
+    }
+}
