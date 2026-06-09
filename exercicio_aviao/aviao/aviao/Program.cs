65e79051bfe53c65992581a7ca7a2ab2042053d8
Tiago Cabral <tiagocabral.gja@gmail.com>
2026-06-08 21:33:32 -0300

    inicialização e conclusão do projeto

---
+++ exercicio_aviao/aviao/aviao/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Numerics;
+
+namespace ex_aviao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] aviao = new int[19, 4];
+            string[,] nomes = new string[19, 4];
+            string[] letras = ["A", "B", "C", "D"];
+            string opcao, nome;
+            int fileira, coluna, ocupados, total;
+            bool tem_nome;
+
+            while (true)
+            {
+                tem_nome = false;
+                Console.Clear();
+                Console.WriteLine("Opções: ");
+                Console.WriteLine("1 - escolher assento");
+                Console.WriteLine("2 - Procurar passageiro");
+                Console.WriteLine("3 - Listagem de ocupação");
+                Console.WriteLine("4 - Verificar se lotada");
+                Console.Write("Digite a opção: ");
+                opcao = Console.ReadLine();
+
+                if (opcao == "1")
+                {
+                    Console.WriteLine("Escolha o assento");
+                    Console.WriteLine("Fileira: ");
+                    fileira = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Coluna: ");
+                    coluna = int.Parse(Console.ReadLine());
+
+
+                    if (aviao[fileira, coluna] != 0)
+                    {
+                        Console.WriteLine("Posição ocupada.");
+                    }
+                    else
+                    {
+                        Console.Write("Digite seu nome: ");
+                        nome = Console.ReadLine();
+                        for (int a = 0; a < nomes.GetLength(0); a++)
+                        {
+                            for (int b = 0; b < nomes.GetLength(1); b++)
+                            {
+                                if (nomes[a, b] == nome)
+                                {
+                                    tem_nome = true;
+                                    break;
+                                }
+                                else
+                                {
+                                    tem_nome = false;
+                                }
+                            }
+                        }
+                        if (tem_nome)
+                        {
+                            Console.WriteLine($"Nome já cadastrado.");
+                        }
+                        else
+                        {
+                            aviao[fileira, coluna] = 1;
+                            nomes[fileira, coluna] = nome;
+                            Console.WriteLine("Assento reservado com sucesso.");
+                        }
+                    }
+                }
+                else if (opcao == "2")
+                {
+                    Console.WriteLine("Procurar passageiro ");
+                    Console.Write("Digite o nome: ");
+                    nome = Console.ReadLine();
+
+                    fileira = coluna = 0;
+
+
+                    for (int a = 0; a < nomes.GetLength(0); a++)
+                    {
+                        for (int b = 0; b < nomes.GetLength(1); b++)
+                        {
+                            if (nomes[a, b] == nome)
+                            {
+                                fileira = a;
+                                coluna = b;
+                                tem_nome = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (tem_nome)
+                    {
+                        Console.WriteLine($"Nome encontrado na posição [{fileira}, {coluna}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nome não cadastrado.");
+                    }
+                }
+                else if (opcao == "3")
+                {
+                    Console.WriteLine("Listagem de ocupação: ");
+                    for (int a = 0; a < nomes.GetLength(0); a++)
+                    {
+                        Console.WriteLine($"Fileira n. {a + 1}");
+                        for (int b = 0; b < nomes.GetLength(1); b++)
+                        {
+                            if (nomes[a, b] != null)
+                            {
+                                Console.WriteLine($"Posição {letras[b]}: {nomes[a, b]}");
+                            }
+                            Console.WriteLine($"Posição {letras[b]}: vazio");
+                        }
+                    }
+                }
+                else if (opcao == "4")
+                {
+                    ocupados = 0;
+                    Console.WriteLine("Verificar se lotada ");
+                    for (int a = 0; a < nomes.GetLength(0); a++)
+                    {
+                        for (int b = 0; b < nomes.GetLength(1); b++)
+                        {
+                            if (nomes[a, b] != null)
+                            {
+                                ocupados++;
+                            }
+                        }
+                    }
+                    total = nomes.GetLength(0) * nomes.GetLength(1);
+                    if (ocupados == total)
+                    {
+                        Console.WriteLine("Todos os assentos estão reservados.");
+                        Console.WriteLine("Encerrando o programa. Volte sempre!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Número de assentos vazios: {total - ocupados}");
+                    }
+                }
+                else
+                {
+                    Console.Write("Opção inválida! Escolha novamente.");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
